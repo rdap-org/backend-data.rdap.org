ae2e78fc1d5bdf4e94ed689a41299641f52c65e8
Gavin Brown <gavin.brown@fastmail.uk>
2026-06-25 12:33:24 +0100

    updated

--- bin/stats.pl
+++ bin/stats.pl
@@ -7,7 +7,7 @@ use JSON::XS;
 use LWP::UserAgent;
 use constant {
     STATS_URL               => q{https://rdap.org/stats},
-    ALERTABLE_QUERY_RATE    => 100,
+    ALERTABLE_QUERY_RATE    => 150,
     PUSHOVER_URL            => q{https://api.pushover.net/1/messages.json},
 };
 use vars qw(@SERIES $DB $UA);
